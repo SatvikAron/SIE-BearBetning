a49ee53c6fba2968cacfd611f74d4d1b8c76766b
Jahurul Islam <jahurul1983@gmail.com>
2017-10-23 14:31:22 +0200

    Final

--- SIE-BearBetningConsoleApp/Program.cs
+++ SIE-BearBetningConsoleApp/Program.cs
@@ -15,11 +15,12 @@ namespace SIE_BearBetningConsoleApp
           var  lines = File.ReadAllLines(@"C:\Users\iceso\source\repos\SIE Bearbetning_Chsarp\SIE-BearBetningConsoleApp\MATTIAS0_SIE4 2015-09-01 - 2016-08-31.SE.txt.txt");
 
             string pattern = @"#TRANS (\d{4}) {.*} (-?\d*.\d*)";
-            //var totalbalce = lines.Where(x => x.Contains("#TRANS")).Select(te => decimal.Parse(Regex.Match(te, pattern).Groups[2].Value)).Sum();
-
             
-            var totalbalce = lines.Where(x => x.Contains("#TRANS")).Select(te => decimal.Parse(Regex.Match(te, pattern).Groups[2].Value, CultureInfo.InvariantCulture)).Sum();
+            var totallines = lines.Where(x => x.Contains("#TRANS")).Count();
 
+            var totalbalce = lines.Where(x => x.Contains("#TRANS")).Select(te => decimal.Parse(Regex.Match(te, pattern).Groups[2].Value, CultureInfo.InvariantCulture)).Sum();
+            Console.WriteLine(totallines);
+            Console.WriteLine("------");
 
             Console.WriteLine(totalbalce);
 
